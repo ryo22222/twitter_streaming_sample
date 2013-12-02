1663d0ba322902a88e97fd4cbf19725daa8c0a4b
Naoto Kaneko <naoty.k@gmail.com>
2013-12-03 00:22:24 +0900

    Escape params

--- app/controllers/streamings_controller.rb
+++ app/controllers/streamings_controller.rb
@@ -1,11 +1,17 @@
 class StreamingsController < ApplicationController
+  include ::ActionView::Helpers::JavaScriptHelper
+
   protect_from_forgery with: :null_session, only: [:create]
 
   def show
   end
 
   def create
-    WebsocketRails[:streaming].trigger "create", params[:tweet]
+    tweet = {}
+    tweet[:text] = escape_javascript(params[:tweet][:text])
+    tweet[:screen_name] = escape_javascript(params[:tweet][:screen_name])
+    tweet[:icon_url] = params[:tweet][:icon_url]
+    WebsocketRails[:streaming].trigger "create", tweet
     head :ok
   end
 end
